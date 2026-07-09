31d5253e9f2ac4c599c1360ba711856d34834f61
jweikinn <weikinn@gruenweikinn.de>
2026-07-09 23:09:27 +0200

    feat: Swap hero background GIF to background_06

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,7 +6,7 @@ export function HeroSection() {
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/background_01.gif"
+        src="/background_06.gif"
         alt=""
         className="absolute inset-0 w-full h-full object-cover"
         style={{ filter: 'blur(8px)', transform: 'scale(1.05)' }}
